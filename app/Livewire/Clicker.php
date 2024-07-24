42f988fe2e62166a7c1ba2e2b1331b46f27fb85e
JalilAbdullayev <abdullacelil00@gmail.com>
2024-07-24 08:07:39 +0400

    Validation

--- app/Livewire/Clicker.php
+++ app/Livewire/Clicker.php
@@ -3,14 +3,19 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Clicker extends Component {
+    #[Rule('required|min:2|max:50')]
     public string $name;
+    #[Rule('required|email|unique:users')]
     public string $email;
+    #[Rule('required|min:5')]
     public string $password;
 
     public function createNewUser() {
+        $this->validate();
         User::create([
             'name' => $this->name,
             'email' => $this->email,
--- resources/views/livewire/clicker.blade.php
+++ resources/views/livewire/clicker.blade.php
@@ -1,9 +1,27 @@
 <div>
-    <form wire:submit="createNewUser">
-        <input wire:model="name" type="text" placeholder="Name"/>
-        <input wire:model="email" type="email" placeholder="E-mail"/>
-        <input wire:model="password" type="password" placeholder="Password"/>
-        <button>
+    <form class="p-5" wire:submit="createNewUser">
+        <input class="block rounded border border-gray-100 px-3 py-1 mb-1" wire:model="name" type="text"
+               placeholder="Name"/>
+        @error('name')
+        <span class="text-red-500 text-xs">
+            {{ $message }}
+        </span>
+        @enderror
+        <input class="block rounded border border-gray-100 px-3 py-1 mb-1" wire:model="email" type="email"
+               placeholder="E-mail"/>
+        @error('email')
+        <span class="text-red-500 text-xs">
+            {{ $message }}
+        </span>
+        @enderror
+        <input class="block rounded border border-gray-100 px-3 py-1 mb-1" wire:model="password" type="password"
+               placeholder="Password"/>
+        @error('password')
+        <span class="text-red-500 text-xs">
+            {{ $message }}
+        </span>
+        @enderror
+        <button class="block rounded px-3 py-1 bg-gray-400 text-white">
             Create
         </button>
     </form>
