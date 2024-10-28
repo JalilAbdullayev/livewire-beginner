2306e6e56e683344835f950b20f771aa8f198b28
Jalil <abdullacelil00@gmail.com>
2024-10-28 15:21:43 +0400

    Offline states

--- resources/views/livewire/user-list.blade.php
+++ resources/views/livewire/user-list.blade.php
@@ -1,4 +1,18 @@
 <div class="p-5 mx-auto max-w-md">
+    <div wire:offline>
+        <div wire:offline.class="mt-10" class="flex items-center p-4 mb-4 text-sm text-yellow-800 border border-yellow-300 rounded-lg bg-yellow-50"
+             role="alert">
+            <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                 fill="currentColor" viewBox="0 0 20 20">
+                <path
+                    d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+            </svg>
+            <span class="sr-only">Info</span>
+            <div>
+                <span class="font-medium">Warning alert!</span> Change a few things up and try submitting again.
+            </div>
+        </div>
+    </div>
     <h2 class="text-2xl mb-3">
         Users List
     </h2>
@@ -7,11 +21,10 @@
     debounce user yazmağı dayandıranda verilmiş müddət keçəndən sonra işləyir.
     throttle hər request arasında verilmiş müddət qədər ara saxlayır.
     --}}
-    <input type="search" wire:model.live.throttle.200ms="search" placeholder="Search"
+    <input type="search" wire:offline.remove wire:model.live.throttle.200ms="search" placeholder="Search"
            class="ring-1 ring-inset ring-gray-300 bg-gray-100 text-gray-900 text-sm rounded block w-full p-2.5
            mx-auto"/>
-    <div role="status" class="my-3 p-4 border border-gray-200 divide-y divide-gray-200 rounded shadow
-    md:p-6">
+    <div role="status" class="my-3 p-4 border border-gray-200 divide-y divide-gray-200 rounded shadow md:p-6">
         @foreach($this->users as $user)
             <div wire:key="{{ $user->id }}" class="flex items-center justify-between py-3">
                 <div>
@@ -22,7 +35,7 @@
                         {{ $user->email }}
                     </div>
                 </div>
-                <button class="text-white bg-teal-500 px-3 py-1 rounded-full">
+                <button wire:offline.attr="disabled" class="text-white bg-teal-500 px-3 py-1 rounded-full">
                     View
                 </button>
             </div>
